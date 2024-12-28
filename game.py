8af10727709355727862ec985300cf35edc3c5ef
piadozo-edriane <doidoiwey@gmail.com>
2024-12-28 11:50:49 +0800

    Update file

--- game.py
+++ game.py
@@ -6,7 +6,45 @@ class Game:
         #general 
         self.surface = pygame.Surface((game_width, game_height))
         self.display_surface = pygame.display.get_surface()
+        self.sprites = pygame.sprite.Group()
+
+        #line for copy
+        self.line_surface = self.surface.copy()
+
+        #tetromino
+        self.tetromino = Tetromino("T", self.sprites)
+    def draw_grid (self):
+        #grid line for height
+        for col in range (1, columns):
+            x = col * cell_size 
+            pygame.draw.line (self.surface, white_color, (x,0), (x, self.surface.get_height()), 1)
+        #grid line for width
+        for row in range (1, rows):
+            y = row * cell_size
+            pygame.draw.line (self.surface, white_color, (0,y), (self.surface.get_width(), y))
+        self.line_surface.blit(self.line_surface, (0, 0))
 
     def run (self):
         #blit stand for block image like layering
+        self.draw_grid()
+        self.sprites.draw(self.surface)
         self.display_surface.blit(self.surface, (padding, padding))
+
+class Block (pygame.sprite.Sprite): # sprite function means 
+    def __init__(self, group, pos, color):
+        super().__init__(group) 
+        self.image = pygame.Surface((cell_size, cell_size))
+        self.image.fill (color)
+  
+        #position
+        self.pos = pygame.Vector2(pos) + pygame.Vector2(3, 5)
+        x = self.pos.x * cell_size
+        y = self.pos.y * cell_size
+        self.rect = self.image.get_rect(topleft = (x, y))
+
+class Tetromino:
+    def __init__(self, shape, group):
+        self.block_positions = tetrominos[shape]["shape"]
+        self.color = tetrominos[shape]["color"]
+
+        self.blocks = [Block(group, pos, self.color) for pos in self.block_positions]
--- settings_game.py
+++ settings_game.py
@@ -12,3 +12,22 @@ score_height_fraction = 1 - preview_height_fraction
 padding = 20
 window_width = game_width + side_bar_width + padding * 3
 window_height = game_height + padding * 2
+#Game behavior
+update_start_speed = 20
+move_wait_time = 200
+rotate_wait_time = 200 
+block_offset = pygame.Vector2(columns // 2, - 1)
+#colors
+white_color = (255, 255, 255)
+
+#Shapes
+
+tetrominos = {
+    "T": {"shape": [(0, 0), (-1, 0), (1, 0), (0,-1)], "color":(3, 115, 252)},
+    "O": {"shape": [(0, 0), (0, -1), (1, 0), (1,-1)], "color":(57, 252, 3)},
+    "J": {"shape": [(0, 0), (0, -1), (0, 1), (-1,1)], "color":(93, 77, 135)},
+    "L": {"shape": [(0, 0), (0, -1), (0, 1), (1, 1)], "color": (122, 59, 16)},
+    "I": {"shape": [(0, 0), (0, 1), (0, -2), (0, 1)], "color": (128, 104, 20)},
+    "S": {"shape": [(0, 0), (-1, 0), (0, -1), (1,-1)], "color": (13, 94, 59)},
+    "Z": {"shape": [(0, 0), (1, 0), (0, -1), (-1,-1)], "color": (173, 52, 111)}
+}
